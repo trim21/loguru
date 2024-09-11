b6cf9f45edf5a6052439e51d4823ac1cd2322463
Trim21 <trim21.me@gmail.com>
2024-09-12 07:04:07 +0800

    wip

--- loguru/_ctime_functions.py
+++ loguru/_ctime_functions.py
@@ -1,14 +1,15 @@
 import os
+from typing import Callable
 
 
-def load_ctime_functions():
+def load_ctime_functions() -> tuple[Callable[[str], float], Callable[[str, float], None]]:
     if os.name == "nt":
         import win32_setctime
 
-        def get_ctime_windows(filepath):
+        def get_ctime_windows(filepath: str) -> float:
             return os.stat(filepath).st_ctime
 
-        def set_ctime_windows(filepath, timestamp):
+        def set_ctime_windows(filepath: str, timestamp: float) -> None:
             if not win32_setctime.SUPPORTED:
                 return
 
@@ -21,23 +22,23 @@ def load_ctime_functions():
 
     if hasattr(os.stat_result, "st_birthtime"):
 
-        def get_ctime_macos(filepath):
+        def get_ctime_macos(filepath: str) -> float:
             return os.stat(filepath).st_birthtime
 
-        def set_ctime_macos(filepath, timestamp):
+        def set_ctime_macos(filepath: str, timestamp: float) -> None:
             pass
 
         return get_ctime_macos, set_ctime_macos
 
     if hasattr(os, "getxattr") and hasattr(os, "setxattr"):
 
-        def get_ctime_linux(filepath):
+        def get_ctime_linux(filepath: str):
             try:
                 return float(os.getxattr(filepath, b"user.loguru_crtime"))
             except OSError:
                 return os.stat(filepath).st_mtime
 
-        def set_ctime_linux(filepath, timestamp):
+        def set_ctime_linux(filepath: str, timestamp: float) -> None:
             try:
                 os.setxattr(filepath, b"user.loguru_crtime", str(timestamp).encode("ascii"))
             except OSError:
@@ -45,13 +46,15 @@ def load_ctime_functions():
 
         return get_ctime_linux, set_ctime_linux
 
-    def get_ctime_fallback(filepath):
+    def get_ctime_fallback(filepath: str) -> float:
         return os.stat(filepath).st_mtime
 
-    def set_ctime_fallback(filepath, timestamp):
+    def set_ctime_fallback(filepath: str, timestamp: float) -> None:
         pass
 
     return get_ctime_fallback, set_ctime_fallback
 
 
 get_ctime, set_ctime = load_ctime_functions()
+
+__all__ = ["get_ctime", "set_ctime"]
